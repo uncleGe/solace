bd1a910848a839c1a6c80d8130c9b13519b8398d
Greg Eisman <gregeisman@gmail.com>
2025-04-23 14:58:32 -0400

    Add Advocate type

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 "use client";
 
+import { Advocate } from "../types/advocate";
 import { useEffect, useState } from "react";
 
 export default function Home() {
-  const [advocates, setAdvocates] = useState([]);
-  const [filteredAdvocates, setFilteredAdvocates] = useState([]);
+  const [advocates, setAdvocates] = useState<Advocate[]>([]);
+  const [filteredAdvocates, setFilteredAdvocates] = useState<Advocate[]>([]);
 
   useEffect(() => {
     console.log("fetching advocates...");
---
+++ src/types/advocate.ts
@@ -0,0 +1,10 @@
+export interface Advocate {
+    id?: number;
+    firstName: string;
+    lastName: string;
+    city: string;
+    degree: string;
+    specialties: string[];
+    yearsOfExperience: number;
+    phoneNumber: number;
+  }
